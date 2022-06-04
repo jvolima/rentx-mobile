baf3860acb520c37082ae33928e5cc610646d806
João Vitor Lima <jvolima2004@gmail.com>
2022-06-04 13:39:21 -0300

    componente Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg'
+import { Car } from '../../components/Car';
 
 import {
   Container,
@@ -12,6 +13,26 @@ import {
 } from './styles'
 
 export function Home(){
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: 120
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png'
+  };
+
+  const carDataTwo = {
+    brand: 'Porsche',
+    name: 'Panamera',
+    rent: {
+      period: 'Ao dia',
+      price: 340
+    },
+    thumbnail: 'https://www.pngkit.com/png/full/237-2375888_porsche_panamera-s.png'
+  };
+
   return(
     <Container>
       <StatusBar
@@ -30,6 +51,9 @@ export function Home(){
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
     </Container>
   );
 }
